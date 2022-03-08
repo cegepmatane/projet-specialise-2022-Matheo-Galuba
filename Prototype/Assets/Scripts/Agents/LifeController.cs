977a815cbf95d3b2b81ce3b340d2e5a697ab362e
Mathéo <matheo.galu56@gmail.com>
2022-03-08 17:32:31 -0500

    Système d'invincibilité pour ne pas perdre trop de dégât en peu de temps

--- Prototype/Assets/Scripts/Agents/LifeController.cs
+++ Prototype/Assets/Scripts/Agents/LifeController.cs
@@ -11,24 +11,24 @@ public class LifeController : MonoBehaviour
     [SerializeField]
     private float zoneDamage = 1.0f;
 
-    private bool isInsideZone = true;
+    private bool isOutsideZone = false;
     private Renderer renderer = null;
     private Color originalColor;
     private IEnumerator takeDamageCoroutine;
+    private bool isInvincible = false;
     private List<Collider> reachableGameObjects = new List<Collider>();
 
     public void Start()
     {
         renderer = GetComponent<Renderer>();
         originalColor = renderer.material.color;
-        StartCoroutine(takeZoneDamage());
     }
 
     public void OnTriggerEnter(Collider other)
     {
         if (other == zoneCollider)
         {
-            isInsideZone = true;
+            isOutsideZone = false;
         }
         if (other.gameObject.tag == "Agent")
         {
@@ -40,7 +40,7 @@ public class LifeController : MonoBehaviour
     {
         if (other == zoneCollider)
         {
-            isInsideZone = false;
+            isOutsideZone = true;
         }
         if (other.gameObject.tag == "Agent")
         {
@@ -48,8 +48,21 @@ public class LifeController : MonoBehaviour
         }
     }
 
+    public void Update()
+    {
+        if (isOutsideZone)
+        {
+            takeDamage(zoneDamage);
+        }
+    }
+
     public void takeDamage(float damage)
     {
+        if (isInvincible)
+        {
+            return;
+        }
+
         life -= damage;
         if (life <= 0.0f)
         {
@@ -67,34 +80,19 @@ public class LifeController : MonoBehaviour
         return life;
     }
 
-    IEnumerator takeZoneDamage()
-    {
-        while (true)
-        {
-            if (isInsideZone == false)
-            {
-                Debug.Log("Damage");
-                takeDamage(zoneDamage);
-
-                // Red color effect
-                // for (float f = 1f; f >= 0; f -= 0.1f)
-                // {
-                //     renderer.material.color = Color.Lerp(originalColor, Color.red, f);
-                //     yield return new WaitForSeconds(0.1f);
-                // }
-
-                // Cooldown
-                yield return new WaitForSeconds(0.5f);
-            }
-        }
-    }
-
     IEnumerator redFlash()
     {
-        for (float f = 1f; f >= 0; f -= 0.1f)
+        isInvincible = true;
+
+        for (float f = 1f; f >= 0; f -= 0.01f)
         {
             renderer.material.color = Color.Lerp(originalColor, Color.red, f);
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(0.01f);
         }
+
+        // Cool down
+        yield return new WaitForSeconds(0.5f);
+
+        isInvincible = false;
     }
 }
