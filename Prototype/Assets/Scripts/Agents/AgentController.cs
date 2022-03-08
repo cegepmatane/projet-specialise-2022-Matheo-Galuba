1eb17d5841e454836b708b4dcfa586dc2bf85f33
Mathéo <matheo.galu56@gmail.com>
2022-03-08 18:01:09 -0500

    Possibilité d'attaquer #8

--- Prototype/Assets/Scripts/Agents/AgentController.cs
+++ Prototype/Assets/Scripts/Agents/AgentController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
@@ -9,20 +11,30 @@ public class AgentController : Agent
     [Header("Movement")]
     [SerializeField]
     private float moveSpeed = 5f;
+
     // Vision parameters
     [Header("Vision")]
     [SerializeField]
     private float viewField = 90f;
+    [SerializeField]
+    private float viewDistance = 10f;
     [SerializeField]
     [Range(3, 30)]
     private uint viewRayNumber = 9;
     [SerializeField]
     private bool showRays = false;
 
+    // Attack parameters
+    [Header("Attack")]
+    [SerializeField]
+    private Collider reachableEnemiesChecker;
+    [SerializeField]
+    private float attackDamage = 1f;
+    private List<Collider> reachableEnemies = new List<Collider>();
+
     public override void OnEpisodeBegin()
     {
-        // Reset the agent position
-        transform.localPosition = new Vector3(0f, 1f, 0f);
+        // Nothing for the moment
     }
 
     // Method called when the neural network request observation data
@@ -30,7 +42,7 @@ public class AgentController : Agent
     {
         // Lunch 3 raycast in fornt of the agent with an angle of 30°
         // Make a layer mask to ignore the "Environment" layer
-        int layerMask = 1 << LayerMask.NameToLayer("Environment");
+        int layerMask = 1 << LayerMask.NameToLayer("Ignore Raycast");
         layerMask = ~layerMask;
         // Eyes position
         Vector3 raycastOrigin = transform.position + transform.forward * 0.51f;
@@ -41,13 +53,13 @@ public class AgentController : Agent
         {
             // Compute the raycast direction
             Vector3 raycastDirection = Quaternion.AngleAxis(-viewField / 2f + (viewField / (viewRayNumber - 1)) * i, transform.up) * transform.forward;
-            Physics.Raycast(raycastOrigin, raycastDirection, out hit, 10.0f, layerMask);
+            Physics.Raycast(raycastOrigin, raycastDirection, out hit, viewDistance, layerMask);
             if (showRays)
             {
                 if (hit.collider)
                     Debug.DrawRay(raycastOrigin, raycastDirection * hit.distance, Color.red);
                 else
-                    Debug.DrawRay(raycastOrigin, raycastDirection * 10.0f, Color.green);
+                    Debug.DrawRay(raycastOrigin, raycastDirection * viewDistance, Color.green);
             }
             sensor.AddObservation(hit.distance);
         }
@@ -66,17 +78,58 @@ public class AgentController : Agent
         // Recieve the action from the neural network
         float moveX = actions.ContinuousActions[0];
         float moveY = actions.ContinuousActions[1];
+        int attackAction = actions.DiscreteActions[0];
 
         // Move the agent
         transform.Translate(0f, 0f, moveY * moveSpeed * Time.deltaTime);
         transform.Rotate(0f, moveX * 180f * Time.deltaTime, 0f);
+
+        // Attack
+        if (attackAction == 1)
+        {
+            if (reachableEnemies.Count > 0)
+            {
+                attack();
+            }
+        }
     }
 
     // Methode called when the user override the agent actions with heuristic mode
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        ActionSegment<int> descreteActions = actionsOut.DiscreteActions;
         continuousActions[0] = Input.GetAxis("Horizontal");
         continuousActions[1] = Input.GetAxis("Vertical");
+
+        descreteActions[0] = Input.GetKey(KeyCode.Space) ? 1 : 0;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Agent"))
+        {
+            reachableEnemies.Add(other);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Agent"))
+        {
+            reachableEnemies.Remove(other);
+        }
+    }
+
+    // Method called when the agent attack an enemy
+    private void attack()
+    {
+        // === Uncomment to debug the agent === //
+        Debug.Log("Attack");
+
+        foreach (Collider enemy in reachableEnemies)
+        {
+            enemy.gameObject.GetComponent<LifeController>().takeDamage(attackDamage);
+        }
     }
 }
